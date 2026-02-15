ac3c6e463d1b4bc7b98706e0e6dd073948e55d2f
ShivaKoushik <shivakoushik7890@gmail.com>
2026-02-15 13:53:41 +0530

    added code files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='Medical_Chatbot',
+    version='1.0',
+    author='Shiva Koushik',
+    description='A medical chatbot that provides information and assistance related to health and medical topics.',
+    author_email='shivakoushik7890@gmail.com',
+    packages=find_packages(),
+    install_requires=[  ]
+)
